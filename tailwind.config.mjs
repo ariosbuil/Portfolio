d1bb4264fd1eeebaae6962de8aaae014e9731be6
Arnau <ariosbuil2002@gmail.com>
2024-02-13 19:16:14 +0100

    Pc to Laptop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundColor: {
+				'quote': '#353536',
+				'card': '#2F2F30',
+				'card-hover': '#353536'
+			},
+			textColor: {
+				'text-hover': '#DE2E03'
+			}
+		},
 	},
 	plugins: [],
 }
